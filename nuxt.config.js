dc7381aaba94341ce212aaf1ca66edaa8cc19272
Jeyhun Hasanov <hasanli.jeyhun@gmail.com>
2022-02-25 08:59:07 +0400

    fix: style files have been structured and reset word-break style on v-card-title element

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,7 +41,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/main.scss'],
     theme: {
       dark: false,
       themes: {
